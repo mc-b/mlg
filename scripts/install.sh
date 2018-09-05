ad4c467064eed83bd1c37be5605d2e78802a8f37
mc-b <marcel.bernet@ch-open.ch>
2018-09-05 10:49:01 +0200

    Tensorflow Image

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,7 +3,7 @@
 #	Installationsscript mlg
 
 # Jupyter Machine Learning Umgebung (zuerst pull, weil Image sehr, sehr gross)
-docker pull jupyter/datascience-notebook
+docker pull jupyter/tensorflow-notebook
 kubectl create -f https://raw.githubusercontent.com/mc-b/duk/master/jupyter/jupyter.yaml 
 
 # Jupyter Scripte etc. Allgemein verfuegbar machen
