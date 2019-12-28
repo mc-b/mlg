455a8ec623db043f18f6e4ac85279f4db0ccb36b
mc-b <marcel.bernet@ch-open.ch>
2019-12-28 20:25:31 +0100

    Tensorflow NB obsolet

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,8 +3,8 @@
 #	Installationsscript mlg
 
 # Jupyter Machine Learning Umgebung (zuerst pull, weil Image sehr, sehr gross)
-docker pull jupyter/tensorflow-notebook
-kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/jupyter/jupyter.yaml 
+#docker pull jupyter/tensorflow-notebook
+#kubectl apply -f https://raw.githubusercontent.com/mc-b/duk/master/jupyter/jupyter.yaml 
 kubectl apply -f https://raw.githubusercontent.com/mc-b/mlg/master/jupyter/jupyter-mlg.yaml
 
 
